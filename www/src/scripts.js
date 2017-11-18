fdfdcc57585fe9a09520182ef9ca12ac9074066d
Jacek Zarski <jacek.zarski@rutgers.edu>
2017-11-17 19:01:12 -0500

    Added content and created email prompt modal after 5 sec

--- www/src/scripts.js
+++ www/src/scripts.js
@@ -1,5 +1,5 @@
-$(function() {
-  var f = function() {
+$(function () {
+  var f = function () {
     var container = $('#scroll-spy-host');
     if (container.is(':visible')) {
       container[$(window).scrollTop() > $('.jumbotron').outerHeight() ? 'addClass' : 'removeClass']('fixed');
@@ -8,25 +8,35 @@ $(function() {
   $(window).scroll(f);
   f();
 });
+
 function underConstruction() {
   alert('This functionality is not available at this moment. Please check later', 'Under construction');
 }
+
 function alert(message, title) {
   var alert = $('.custom-alert');
   alert.find('h4').text(title || '');
   alert.find('p.text').text(message || '');
   alert.show();
 }
+
+setTimeout(function () {
+  $('#myModal').modal('show');
+}, 5000);
+
 function setLoading(state) {
   state ? $('.loading').show() : $('.loading').hide();
 }
-$('.custom-alert button').click(function () { $('.custom-alert').hide(); });
 
-$('form').submit(function(e) {
+$('.custom-alert button').click(function () {
+  $('.custom-alert').hide();
+});
+
+$('form').submit(function (e) {
   var form = $(this),
-    sorryTitle = "We are very sorry, but...",
-    sorryMessage = "Your request is malformed. You should specify at least email and message body to be able to sent an email";
-  
+    sorryTitle = 'We are very sorry, but...',
+    sorryMessage = 'Your request is malformed. You should specify at least email and message body to be able to sent an email';
+
   if (!form.find('#email').val() || !form.find('#text').val()) {
     alert(sorryMessage, sorryTitle);
   }
@@ -37,14 +47,14 @@ $('form').submit(function(e) {
       type: form.attr('method') || 'POST',
       url: form.attr('action'),
       data: data,
-      success: function(data) {
+      success: function (data) {
         setLoading(false);
-        alert("Your email was sent successfully. Our marketing department started processing your request and will call you later", "Great! Thank you");
+        alert('Your email was sent successfully. Our marketing department started processing your request and will call you later', 'Great! Thank you');
         form.trigger('reset');
       },
-      error: function(jqXHR, message, status) {
+      error: function (jqXHR, message, status) {
         setLoading(false);
-        alert(jqXHR.status === 400 ? sorryMessage : "The server is busy. Please try again later.", sorryTitle);
+        alert(jqXHR.status === 400 ? sorryMessage : 'The server is busy. Please try again later.', sorryTitle);
       }
     });
   }
